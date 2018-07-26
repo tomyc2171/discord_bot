4d7da20393e9aee58ab1ec8734f19d4276ab3283
Joshua Tavassolikhah <joshtavassolikhah@yahoo.com>
2018-07-26 16:31:02 -0700

    organized a bit

--- bot.py
+++ bot.py
@@ -1,52 +1,53 @@
 import discord
 from discord.ext import commands
 
-token = 'NDcxOTYxNjU3ODgzNzU0NDk3.Djscfw.prkRhd_XrJZsM9pNVVNsIQAm7Mc'
+from config import help_message, token, life_time
 
 """
-https://discordapp.com/api/oauth2/authorize?client_id=471961657883754497&permissions=0&scope=bot
+Some background:
+The "ctx" parameter in each function represents 
+a "context" object. This object allows discord
+to know the context of when the command was used.
+For example, if I use the command and I am in the 
+general text channel, then the context parameter
+holds that information, so the bot knows to 
+respond in that general text channel.
+
+As for the pass_context=True thing, for some reason
+all of the examples had that, and I am still working
+on *why* that is needed. Lmk if you have any questions.
 """
 
-help_message = """
-
-Test Bot!
-
-A bot we can use for testing purposes.
-
-!hello ->
-	says hello
-
-!purge ->
-    Mass Delete Messages that have key prefixes.
-    For testing purposes.
-    Right now, it deletes all messages with '!' in it
-
-!voice ->
-    For joining/leaving the author's voice channel
-
-    Simply joins and leaves the voice channel, for now
-    Maybe we can use this to plays songs, potentially?
-
-    Example:
-
-    Me: !voice
-    Bot: Joining Your Voice Channel
-
-    Me: !voice
-    Bot: Leaving Your Voice Channel
-
-!help ->
-	This message.
-
-"""
-
+# needed for putting bot in and out of voice channel
 current_voice = None
 
-life_time = 10
+"""
+this creates a command bot. A bit different from the bot
+we made in the tutorial. This is a bot that is meant just
+for commands, whereas, I think, the one in the tutorial 
+is for conversations. Why this is useful is because we
+can create command functions which imo look a lot cleaner
+than the whole "if message.startswith('!')" stuff.
 
-bot = commands.Bot(command_prefix='!', description = "says hello")
+For example, look at the hello command, which is so much 
+cleaner than the one in the tutorial. It's just one line,
+yung pattis likes that.
+"""
+bot = commands.Bot(command_prefix='?', description = "test bot")
+
 # Remove default help command, created our own 
 bot.remove_command('help')
+
+def get_help_page(ctx) -> str:
+    """
+    creates the help page and returns it
+    Just a helper function I made
+    """
+
+    begin = bot.formatter.format_help_for(ctx, ctx.command)[0][:4]
+    end = bot.formatter.format_help_for(ctx, ctx.command)[0][-4:]
+
+    return (begin + help_message + end)
 
 @bot.command(pass_context = True)
 async def help(ctx):
@@ -61,17 +62,17 @@ async def hello(ctx):
     """
     await ctx.bot.say("hello")
 
-@bot.event
-async def on_command_error(error, ctx):
+@bot.command(pass_context=True)
+async def purge(ctx, *args):
     """
-    For if a user says a wrong command, or
-    an error occurs when a command is inputted.
+    Mass Delete Messages that have key prefixes
 
-    This gives them the help page
+    For testing purposes
     """
-
-    await bot.send_message(ctx.message.channel, 'oh my, here are the correct commands')
-    await bot.send_message(ctx.message.channel, get_help_page(ctx))
+    channel = ctx.message.channel
+    check = lambda msg: msg.content == "" or msg.content[0] in ['!', '&', '?'] # prefixes for bots
+    await ctx.bot.purge_from(channel, limit=1000, check=check)
+    await ctx.bot.say("Messages have been purged", delete_after=life_time)
 
 
 @bot.command(pass_context=True)
@@ -109,18 +110,6 @@ async def voice(ctx):
         current_voice = await ctx.bot.join_voice_channel(channel)
         await ctx.bot.say("Joining Your Voice Channel")
 
-@bot.command(pass_context=True)
-async def purge(ctx, *args):
-    """
-    Mass Delete Messages that have key prefixes
-
-    For testing purposes
-    """
-    channel = ctx.message.channel
-    check = lambda msg: msg.content == "" or msg.content[0] in ['!']
-    await ctx.bot.purge_from(channel, limit=1000, check=check)
-    await ctx.bot.say("Messages have been purged", delete_after=life_time)
-
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -128,15 +117,17 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-def get_help_page(ctx) -> str:
+@bot.event
+async def on_command_error(error, ctx):
     """
-    creates the help page and returns it
+    For if a user says a wrong command, or
+    an error occurs when a command is inputted.
+
+    This gives them the help page
     """
 
-    begin = bot.formatter.format_help_for(ctx, ctx.command)[0][:4]
-    end = bot.formatter.format_help_for(ctx, ctx.command)[0][-4:]
-
-    return (begin + help_message + end)
+    await bot.send_message(ctx.message.channel, 'oh my, here are the correct commands')
+    await bot.send_message(ctx.message.channel, get_help_page(ctx))
 
 if __name__ == '__main__':
 	bot.run(token)
---
+++ config.py
@@ -0,0 +1,51 @@
+"""
+config.py
+
+data needed for bot
+"""
+
+"""
+for authorizing
+https://discordapp.com/api/oauth2/authorize?client_id=471961657883754497&permissions=0&scope=bot
+"""
+
+help_message = """
+
+Test Bot!
+
+A bot we can use for testing purposes.
+
+!hello ->
+	says hello
+
+!purge ->
+    Mass Delete Messages that have key prefixes.
+    For testing purposes.
+    Right now, it deletes all messages with '!',
+    '?', and '&' in it
+
+!voice ->
+    For joining/leaving the author's voice channel
+
+    Simply joins and leaves the voice channel, for now
+    Maybe we can use this to plays songs, potentially?
+
+    Example:
+
+    Me: !voice
+    Bot: Joining Your Voice Channel
+
+    Me: !voice
+    Bot: Leaving Your Voice Channel
+
+!help ->
+	This message.
+
+"""
+
+token = 'NDcxOTYxNjU3ODgzNzU0NDk3.Djscfw.prkRhd_XrJZsM9pNVVNsIQAm7Mc'
+
+# message life time
+life_time = 10
+
+
